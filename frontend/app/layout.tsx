3747183d3b57cd3c53769b700509eafed389387b
Abdullah Hashim <ahashim@rega.gov.sa>
2025-03-02 17:36:43 +0000

    Update layout metadata for GSoC 2025 projects and integrate Vercel Analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,24 +1,33 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-
-const inter = Inter({ subsets: ["latin"] })
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Organizations & Projects Explorer",
-  description: "Explore organizations and their projects",
-}
+  title: "GSoC 2025 Projects",
+  description:
+    "a list of all GSoC 2025 projects web scraped from the GSoC website",
+  keywords: [
+    "GSoC",
+    "Google Summer of Code",
+    "2025",
+    "Projects",
+    "GSoC scraped projects",
+  ],
+  applicationName: "GSoC 2025 Projects",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
-  )
+  );
 }
-
